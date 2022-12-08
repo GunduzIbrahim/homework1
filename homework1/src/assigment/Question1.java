ef8fa8d51333c0fb1e342a0b2bd8b90da5a61e6e
ihsan_elif <ihsan_elif@DESKTOP-60VG5N8>
2022-12-08 17:39:56 +0100

    Question1 second fix commit

--- homework1/src/assigment/Question1.java
+++ homework1/src/assigment/Question1.java
@@ -12,11 +12,10 @@ public class Question1 {
 		
 		System.out.println("First Worker age is : " + ageWorker1);
 		System.out.println("Second Worker age is : " + ageWorker2);
-		System.out.println("Third Worker age is : " + ageWorker3);
+		System.out.println("Third Worker age is : " + ageWorker3);	
 		
 		
-		// short data types
-		
+		// short data types		
 		
 		short firstWorkerIdNumber = 365;
 		short secondWorkerIdNumber = 214;
@@ -25,6 +24,7 @@ public class Question1 {
 		System.out.println("\nFirst Worker ID number is : " + firstWorkerIdNumber);
 		System.out.println("Second Worker ID number is : " + secondWorkerIdNumber);
 		System.out.println("Third Worker ID number is : " + thirdWorkerIdNumber);
+		
 		
 		// integer data types
 		
@@ -36,6 +36,7 @@ public class Question1 {
 		System.out.println("Second Worker wage is : " + secondWorkerWage + " $");
 		System.out.println("Third Worker wage is : " + thirdWorkerWage + " €");
 		
+		
 		// long data types
 		
 		long firstWorkerTaxNumber = 351556486445L;
@@ -45,6 +46,7 @@ public class Question1 {
 		System.out.println("\nFirst Worker tax number is : " + firstWorkerTaxNumber);
 		System.out.println("Second Worker tax number is : " + secondWorkerTaxNumber);
 		System.out.println("Third Worker tax number is : " + thirdWorkerTaxNumber);
+		
 		
 		// float data types		
 		
@@ -56,6 +58,7 @@ public class Question1 {
 		System.out.println("Second Worker height is : " + secondWorkerHeight + " cm");
 		System.out.println("Third Worker height is : " + thirdWorkerHeight + " cm");
 		
+		
 		// double data types
 		
 		double firstWorkerHourlyWage = 4.584548;
@@ -66,6 +69,7 @@ public class Question1 {
 		System.out.println("First Worker hourly wage is : " + secondWorkerHourlyWage + " $");
 		System.out.println("First Worker hourly wage is : " + thirdWorkerHourlyWage + " €");
 		
+		
 		// boolean data types
 		
 		boolean firstWorkerJobSatisfaction = false;
@@ -75,6 +79,7 @@ public class Question1 {
 		System.out.println("\nFirst Worker is happy ? : " + firstWorkerJobSatisfaction);
 		System.out.println("Second Worker is happy ? : " + secondWorkerJobSatisfaction);
 		System.out.println("Third Worker is happy ? : " + thirdWorkerJobSatisfaction);
+		
 		
 		// char data types
 		
